9a451b12a438e4ef698f343b3af2ff8c1e257324
Rony Cruz <ronald.cruz.reynoso@gmail.com>
2026-09-19 22:56:41 -0400

    Implementar Angular Router con 5 rutas y navegación en el Header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './components/inicio/inicio';
+import { SobreMi } from './components/sobre-mi/sobre-mi';
+import { Habilidades } from './components/habilidades/habilidades';
+import { Proyectos } from './components/proyectos/proyectos';
+import { Contacto } from './components/contacto/contacto';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Inicio },
+  { path: 'sobre-mi', component: SobreMi },
+  { path: 'habilidades', component: Habilidades },
+  { path: 'proyectos', component: Proyectos },
+  { path: 'contacto', component: Contacto },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
-import { Inicio } from './components/inicio/inicio';
-import { SobreMi } from './components/sobre-mi/sobre-mi';
-import { Habilidades } from './components/habilidades/habilidades';
-import { Proyectos } from './components/proyectos/proyectos';
-import { Contacto } from './components/contacto/contacto';
 import { Footer } from './components/footer/footer';
 
 @Component({
-  imports: [RouterOutlet, Header, Inicio, SobreMi, Habilidades, Proyectos, Contacto, Footer],
+  imports: [RouterOutlet, Header, Footer],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
